874c9d467ab31837494aa93c6833435748f58406
Ibnu Fajar Yunardi <ibnu.fy@gmail.com>
2018-04-16 00:38:09 +0700

    Update Some Dependencies

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,10 @@
+require 'selenium-webdriver'
+require 'rubygems'
 require 'allure-cucumber'
+
+AllureCucumber.configure do |c|
+   c.output_dir = "/output/report"
+end
+AllureCucumber.configure do |c|
+  c.clean_dir  = true
+end
