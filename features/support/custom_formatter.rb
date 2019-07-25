66ae99bab7906e7c2ed68daf586486ce9b92ed1f
Ibnu Fajar Yunardi <ifyunardi@binar.co.id>
2019-07-25 17:10:42 +0700

    update depend

---
+++ features/support/custom_formatter.rb
@@ -0,0 +1,7 @@
+class Cucumber::Core::Test::Step
+  def name
+    return text if self.text == 'Before hook'
+    return text if self.text == 'After hook'
+    "#{source.last.keyword}#{text}"
+  end
+end
